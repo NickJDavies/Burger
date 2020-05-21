05f2245a2e8f2b1e0b516651e8390f154e6e2add
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-05-21 15:23:55 +0800

    handlebars dont want to work sooo

--- public/js/view.js
+++ public/js/view.js
@@ -4,11 +4,7 @@ $(document).ready(function() {
   // Our new burgers will go inside the burgerContainer
   var $burgerContainer = $(".burger-container");
   // Adding event listeners for deleting, editing, and adding burgers
-  $(document).on("click", "button.delete", deleteBurger);
-  $(document).on("click", "button.complete", toggleComplete);
-  $(document).on("click", ".burger-item", editBurger);
-  $(document).on("keyup", ".burger-item", finishEdit);
-  $(document).on("blur", ".burger-item", cancelEdit);
+  $(document).on("click", "button.delete", eatBurger);
   $(document).on("submit", "#burger-form", insertBurger);
 
   // Our initial burgers array
@@ -35,92 +31,42 @@ $(document).ready(function() {
     });
   }
 
-  // This function deletes a burger when the user clicks the delete button
-  function deleteBurger(event) {
+  // This function eats a burger when the user clicks the eat button
+  function eatBurger(event) {
     event.stopPropagation();
     var id = $(this).data("id");
     $.ajax({
-      method: "DELETE",
+      method: "PUT",
       url: "/api/burgers/" + id
     }).then(getBurgers);
   }
 
-  // This function handles showing the input box for a user to edit a burger
-  function editBurger() {
-    var currentBurger = $(this).data("burger");
-    $(this).children().hide();
-    $(this).children("input.edit").val(currentBurger.text);
-    $(this).children("input.edit").show();
-    $(this).children("input.edit").focus();
-  }
-
-  // Toggles complete status
-  function toggleComplete(event) {
-    event.stopPropagation();
-    var burger = $(this).parent().data("burger");
-    burger.complete = !burger.complete;
-    updateBurger(burger);
-  }
-
-  // This function starts updating a burger in the database if a user hits the "Enter Key"
-  // While in edit mode
-  function finishEdit(event) {
-    var updatedBurger = $(this).data("burger");
-    if (event.which === 13) {
-      updatedBurger.text = $(this).children("input").val().trim();
-      $(this).blur();
-      updateBurger(updatedBurger);
-    }
-  }
-
-  // This function updates a burger in our database
-  function updateBurger(burger) {
-    $.ajax({
-      method: "PUT",
-      url: "/api/burgers",
-      data: burger
-    }).then(getBurgers);
-  }
-
-  // This function is called whenever a burger item is in edit mode and loses focus
-  // This cancels any edits being made
-  function cancelEdit() {
-    var currentBurger = $(this).data("burger");
-    if (currentBurger) {
-      $(this).children().hide();
-      $(this).children("input.edit").val(currentBurger.text);
-      $(this).children("span").show();
-      $(this).children("button").show();
-    }
-  }
-
-  // This function constructs a burger-item row
   function createNewRow(burger) {
     var $newInputRow = $(
       [
         "<li class='list-group-item burger-item'>",
-        "<span>",
-        burger.text,
-        "</span>",
-        "<input type='text' class='edit' style='display: none;'>",
-        "<button class='delete btn btn-danger'>x</button>",
-        "<button class='complete btn btn-primary'>✓</button>",
+          "<span>",
+            burger.text,
+          "</span>",
+          "<button class='delete btn btn-danger'>EAT</button>",
         "</li>"
       ].join("")
     );
 
     $newInputRow.find("button.delete").data("id", burger.id);
-    $newInputRow.find("input.edit").css("display", "none");
     $newInputRow.data("burger", burger);
     if (burger.complete) {
       $newInputRow.find("span").css("text-decoration", "line-through");
+      $newInputRow.find("button.delete").css("display", "none");
     }
     return $newInputRow;
   }
 
-  // This function inserts a new burger into our database and then updates the view
   function insertBurger(event) {
     event.preventDefault();
+    if ($newItemInput.val() === "") {
+      return;
+    }
     var burger = {
       text: $newItemInput.val().trim(),
       complete: false
--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
+// Dependencies
 // =============================================================
 const express = require("express");
-
-// Sets up the Express App
+// =============================================================
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
+// =============================================================
 
-// Requiring our models for syncing
+
+// sets up the server syncing
+// =============================================================
 const db = require("./models");
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Static directory
 app.use(express.static("public"));
+// =============================================================
 
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
+// =============================================================
 
-// Syncing our sequelize models and then starting our Express app
+// syncing the models folder
 // =============================================================
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+// =============================================================
